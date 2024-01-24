8db73ac8c95d96b702b5ef0b844d7b02cf7cfc76
MelihKrts <melih.karatas1281@gmail.com>
2024-01-24 13:16:55 +0300

    started 30 days of python exercise

---
+++ Day_01_Introduction/Day_01_Exercise/Exercise_1.py
@@ -0,0 +1,34 @@
+# Exercise Level 2 Solution 
+
+# Exercise Level 1 terminal exercise 
+
+# 2.1
+
+import sys
+print(sys.version)
+
+# 2.2
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3 % 4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#2.3
+print("Melih")
+print("Karatas")
+print("Turkey")
+print("I am enjoying 30 days of python")
+
+# 2.4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["Asabeneh","Python","Finland"]))
+print(type("Melih"))
+print(type("Karatas"))
+print(type("Turkey"))
---
+++ Day_01_Introduction/Day_01_Exercise/Exercise_2.py
@@ -0,0 +1,54 @@
+# Exercise Level 3 Solution
+
+# 3.1
+
+# Number
+import math
+int = 100
+print(int)
+
+float = 2.95
+print(float)
+
+complex = 1j
+print(complex)
+
+# String
+string = "It is a example string text"
+print(string)
+
+# Boolean
+my_boolean = True
+print(f"It is light on {my_boolean}")
+
+# List
+list = [0, 1, 2, 3, 3.96, "Turkey", "Apple", False]
+print(list)
+
+# Tuple
+print(("Mercury", "Venus", "Earth", "Mars"))  # planets
+
+# Set
+print((27, 81, 9, 3))
+
+# Dictionary
+print({
+    "firstName": "Melih",
+    "lastName": "Karatas",
+    "age": 23,
+    "skiils": ["JS", "React", "Python"]
+})
+
+# Euclidian distance
+
+
+def euclidian(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+dot1 = (2, 3)
+dot2 = (10, 8)
+
+distance = euclidian(dot1[0], dot1[1], dot2[0], dot2[1])
+print(f"Euclidean distance between two points: {distance}")
+
+
