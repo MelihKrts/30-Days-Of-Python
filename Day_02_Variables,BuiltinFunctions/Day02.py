debfd2473d42c163dabe39276d5d9d0e25d43665
MelihKrts <melih.karatas1281@gmail.com>
2024-01-25 11:51:56 +0300

    refactor:[edited Day02.py file codes]

--- Day_02_Variables,BuiltinFunctions/Day02.py
+++ Day_02_Variables,BuiltinFunctions/Day02.py
@@ -39,8 +39,8 @@ print("Married", is_married)
 
 # Input
 
-# first_name = input("What is your name: ")
-# age = input("How old are you?: ")
+first_name = input("What is your name: ")
+age = input("How old are you?: ")
 
 print(first_name)
 print(age)
