e2589bc78597b78765d7a7c135d71503bf5488be
MelihKrts <melih.karatas1281@gmail.com>
2024-02-10 11:20:06 +0300

    added Day 17 Exception Handling exercise solution

---
+++ Day_17_Exception_Handling/Day_17_Exercise/Day_17_exercise.py
@@ -0,0 +1,6 @@
+# Day 17 Exercise Solution
+
+# 1
+names = ["Finland", "Sweden", "Norway", "Denmark", "Iceland", "Estonia", "Russia"]
+*nordic_countries, es, ru = names
+print(nordic_countries, es, ru)
